6570b66d70df421b30831e1b69738524df22aa1a
MikChanna <channa.mik@gmail.com>
2020-06-25 21:56:46 -0400

    added input area and save button in row.

--- script.js
+++ script.js
@@ -7,15 +7,24 @@ $(document).ready(function () {
 
   currentDay.text(now);
 
-  //
+  //create rows for every hour of the day
   for (var i = 0; i < 24; i++) {
     createDiv();
 
     function createDiv() {
       var time = moment().hour(i).format("H");
-      var plannerRow = $("<div class = 'daily'>");
+      var plannerRow = $("<div class = 'daily row'>");
       plannerRow.attr("data-time", time);
       container.append(plannerRow);
     }
   }
+  // adding new divs within the row
+  var rowDiv = $("<div class = 'hourOfDay col-md-3'><span id= 'hour'>");
+  var input = $(
+    "<div class = 'inputInfo col-md-6'><input type = 'text' id = 'input'>"
+  );
+  var save = $(
+    "<div class = 'save col-md-3'><button class = 'saveBtn'>Save<i class='fa fa-calendar-plus-o'></i></button>"
+  );
+  $(".daily").append(rowDiv, input, save);
 });
